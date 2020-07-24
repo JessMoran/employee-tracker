77afe9bbaa9e21e49de689b7dacb6f02edb09831
Jess Moran <jessmoran.dev@gmail.com>
2020-07-24 00:42:41 -0400

    Fix some options

---
+++ Dev/squema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+
+Production
+
+Research and Development
--- server.js
+++ server.js
@@ -28,24 +28,23 @@ function promptUserOpts() {
     message: "What would you like to do?",
     name: "opts",
     choices: [
-      "View all departments",
+      "View departments",
       "Add department",
       "Update department",
       "Remove department",
-      "View all employees",
+      "View employees",
       "Add employee",
       "Update employee's manager",
       "Remove employee",
-      "View all roles",
+      "View roles",
       "Add role",
-      "Update role",
       "Remove role",
       "Quit"
     ]
   }).then(function (answer) {
     switch (answer.opts[0]) {
       case "View departments":
-        viewCol("departments");
+        viewTable("departments");
         break;
 
       case "Add department":
@@ -60,8 +59,8 @@ function promptUserOpts() {
         removeDepartment("departments");
         break;
 
-      case "View all employees":
-        viewCol("employees");
+      case "View employees":
+        viewTable("employees");
         break;
 
       case "Add employee":
@@ -76,20 +75,16 @@ function promptUserOpts() {
         updateEmployee("employees");
         break;
 
-      case "View all roles":
-        viewCol("roles");
+      case "View roles":
+        viewTable("roles");
         break;
 
       case "Add role":
         addNewRole("roles")
         break;
 
-      case "Update role":
-        updateRole("roles");
-        break;
-
       case "Remove role":
-        removeRole(["id", "first_name"], "roles");
+        removeRole("roles");
         break;
 
       case "Quit":
@@ -99,15 +94,15 @@ function promptUserOpts() {
   });
 }
 
-function viewCol(col) {
-  connection.query("SELECT * FROM ??", [col], function (err, res) {
+function viewTable(table) {
+  connection.query("SELECT DISTINCT * FROM ??", [table], function (err, res) {
     if (err) throw err;
     console.table(res);
     connection.end();
   });
 }
 
-function addNewDepartment(col) {
+function addNewDepartment(table) {
   return inquirer.prompt([
     {
       type: "input",
@@ -115,24 +110,44 @@ function addNewDepartment(col) {
       name: "name"
     }
   ]).then(function (answer) {
-    insertNewEl(col, answer);
+    insertNewEl(table, answer);
   })
 }
 
-function addNewRole(col) {
+function addNewRole(table) {
   return inquirer.prompt([
     {
       type: "input",
       message: "Type the new role",
       name: "title"
+    },
+    {
+      type: "input",
+      message: "Type the salary",
+      name: "salary"
     }
   ]).then(function (answer) {
-    insertNewEl(col, answer);
+    connection.query("SELECT DISTINCT id,name FROM departments", function (err, res) {
+      return inquirer.prompt([
+        {
+          type: "checkbox",
+          message: "Type the salary",
+          name: "department",
+          choices: res.map(department => department.name)
+        }
+      ]).then(function (answer2) {
+        res.forEach(department => {
+          if (department.name === answer2.department[0]) {
+            answer['department_id'] = department.id;
+          }
+        })
+        insertNewEl(table, answer);
+      })
+    })
   })
 }
 
-function addNewEmployee(col) {
-  let answers = [];
+function addNewEmployee(table) {
   return inquirer.prompt([
     {
       type: "input",
@@ -145,8 +160,7 @@ function addNewEmployee(col) {
       name: "last_name"
     },
   ]).then(function (answer) {
-    answers.push(answer)
-    connection.query("SELECT roles.id, roles.title FROM roles", function (err, res) {
+    connection.query("SELECT DISTINCT roles.id, roles.title FROM roles", function (err, res) {
       return inquirer.prompt([
         {
           type: "checkbox",
@@ -157,10 +171,10 @@ function addNewEmployee(col) {
       ]).then(function (answer2) {
         res.forEach(role => {
           if (role.title === answer2.role[0]) {
-            answers.push(role.id);
+            answer['role_id'] = role.id;
           }
         })
-        connection.query("SELECT employee2.id,employee2.first_name FROM employees as employee1 JOIN employees as employee2 on employee1.manager_id = employee2.id", function (err, res) {
+        connection.query("SELECT DISTINCT employee2.id,employee2.first_name FROM employees as employee1 JOIN employees as employee2 on employee1.manager_id = employee2.id", function (err, res) {
           return inquirer.prompt([
             {
               type: "checkbox",
@@ -171,11 +185,10 @@ function addNewEmployee(col) {
           ]).then(function (answer3) {
             res.forEach(employee => {
               if (employee.first_name === answer3.manager[0]) {
-                answers.push(employee.id);
+                answer['manager_id'] = employee.id;
               }
             })
-            console.log(col, answers)
-            insertNewEl(col, answers);
+            insertNewEl(table, answer);
           })
         })
       })
@@ -183,170 +196,156 @@ function addNewEmployee(col) {
   })
 }
 
-function insertNewEl(col, answers) {
-  connection.query("INSERT INTO ?? SET ?", [col, answers], function (err) {
+function insertNewEl(table, answers) {
+  connection.query("INSERT INTO ?? SET ?", [table, answers], function (err) {
     if (err) throw err;
     connection.end();
   });
 }
 
-function removeEmployee(col) {
-  connection.query("SELECT ?? FROM ?? ", [["id", "first_name"], col], function (err, res) {
+function removeEmployee(table) {
+  connection.query("SELECT DISTINCT ?? FROM ?? ", [["id", "first_name"], table], function (err, res) {
     return inquirer.prompt([
       {
         type: "checkbox",
         message: "Which employee do you want to delete?",
-        name: "name",
+        name: "first_name",
         choices: res.map(element => element.first_name),
       },
     ]).then(function (answer) {
+      let employeeId;
       res.filter(employee => {
-        if (employee.first_name === answer.name[0]) {
-          console.log(employee.id);
+        if (employee.first_name === answer.first_name[0]) {
+          employeeId = {'id': employee.id}
+          removeEl(table, employeeId);
+          console.log("Removed");
         }
       })
-      removeEl(col, id)
     })
   })
 }
 
-function removeRole(col) {
-  connection.query("SELECT ?? FROM ?? ", [["id", "title"], col], function (err, res) {
+function removeRole(table) {
+  connection.query("SELECT DISTINCT id,title FROM ?? ", [table], function (err, res) {
+    let id;
     return inquirer.prompt([
       {
         type: "checkbox",
         message: "Which role do you want to delete?",
-        name: "role",
-        choices: res.map(element => element.title),
+        name: "title",
+        choices: res.map(role => role.title),
       },
     ]).then(function (answer) {
       res.filter(role => {
-        if (role.title === answer.role[0]) {
-          console.log(role.id);
+        if (role.title === answer.title[0]) {
+          id = {'id': role.id}
+          console.log("Removed");
         }
       })
-      removeEl(col, id);
+      removeEl(table, id);
     })
   })
 }
 
-function removeDepartment(col) {
-  connection.query("SELECT ?? FROM ?? ", [["id", "name"], col], function (err, res) {
+function removeDepartment(table) {
+  connection.query("SELECT DISTINCT id,name FROM ?? ", [table], function (err, res) {
     return inquirer.prompt([
       {
         type: "checkbox",
         message: "Which department do you want to delete?",
-        name: "department",
+        name: "name",
         choices: res.map(element => element.name),
       },
     ]).then(function (answer) {
+      let id = {}
       res.filter(department => {
-        if (department.name === answer.department[0]) {
-          console.log(department.id);
+        if (department.name === answer.name[0]) {
+          id = {id: department.id}
         }
       })
-      removeEl(col, id);
+      removeEl(table, id);
     })
   })
 }
 
-function removeEl(col, id) {
-  connection.query("DELETE FROM ?? WHERE ?", [col, id], function (err) {
+function removeEl(table, id) {
+  connection.query("DELETE FROM ?? WHERE ?", [table, id], function (err) {
     if (err) throw err;
     connection.end();
   })
 }
 
-function updateRole(col) {
-  connection.query("SELECT ?? FROM ?? ", [["id", "title"], col], function (err, res) {
-    return inquirer.prompt([
-      {
-        type: "checkbox",
-        message: "What role would you like to update?",
-        name: "role",
-        choices: res.map(element => element.title),
-      },
-    ]).then(function (answer) {
-      res.filter(role => {
-        if (role.title === answer.role[0]) {
-          console.log(role.id);
-        }
-      })
-      return inquirer.prompt([
-        {
-          type: "input",
-          message: "Type the new role",
-          name: "title",
-        },
-      ]).then(function (answer) {
-        updateEl(col, answer, id);
-      })
-    })
-  })
-}
-
-function updateDepartment(col) {
-  connection.query("SELECT ?? FROM ?? ", [["id", "name"], col], function (err, res) {
+function updateDepartment(table) {
+  connection.query("SELECT DISTINCT id,name FROM ?? ", [table], function (err, res) {
     return inquirer.prompt([
       {
         type: "checkbox",
         message: "What department would you like to update?",
-        name: "department",
+        name: "name",
         choices: res.map(element => element.name),
       },
     ]).then(function (answer) {
-      let answer3 = [];
+      let id;
       res.filter(department => {
-        if (department.title === answer.department[0]) {
-          answer3.push(department.id);
+        if (department.name === answer.name[0]) {
+          id = {'id':department.id}
         }
       })
-      answer3.push(answers);
       return inquirer.prompt([
         {
           type: "input",
           message: "Type the new department",
-          name: "title",
+          name: "name",
         },
-      ]).then(function (answer) {
-        updateEl(col, answer, id);
+      ]).then(function (answer2) {
+        updateEl(table,answer2,id)
       })
     })
   })
 }
 
-function updateEmployee(col) {
-  connection.query("SELECT ?? FROM ?? ", ["first_name", col], function (err, res) {
+function updateEmployee(table) {
+  let managerId;
+  let employeeId;
+  connection.query("SELECT DISTINCT id,first_name, manager_id FROM employees", function (err, res) {
     return inquirer.prompt([
       {
         type: "checkbox",
         message: "Which employee's manager do you want to update?",
-        name: "manager",
+        name: "first_name",
         choices: res.map(element => element.first_name),
       },
     ]).then(function (answer) {
-      let answer3 = [];
-      res.filter(department => {
-        if (department.title === answer.manager[0]) {
-          answer3.push(department.id);
+      res.map(employee => {
+        if (employee.first_name === answer.first_name[0]) {
+          managerId = {'manager_id': employee.manager_id}
+          employeeId = {'id': employee.id}
         }
       })
-      answer3.push(answers);
-      return inquirer.prompt([
-        {
-          type: "input",
-          message: "Type the new manager",
-          name: "manager_id",
-        },
-      ]).then(function (answer) {
-        updateEl(col, answer, id);
+      connection.query("SELECT DISTINCT employee2.id,employee2.first_name FROM employees as employee1 JOIN employees as employee2 on employee1.manager_id = employee2.id", function (err, res) {
+        let newManagerId;
+        return inquirer.prompt([
+          {
+            type: "checkbox",
+            message: "Which employee's manager do you want to update?",
+            name: "manager",
+            choices: res.map(element => element.first_name),
+          },
+        ]).then(function (answer) {
+          res.forEach(manager => {
+            if (manager.first_name === answer.manager[0]) {
+              newManagerId = {'manager_id': manager.id}
+            }
+          })
+          updateEl(table, newManagerId, employeeId);
+        })
       })
     })
   })
 }
 
-function updateEl(col, id) {
-  connection.query("UPDATE ?? SET ? WHERE ?", [col, answer, id], function (err) {
+function updateEl(table, answer2, answer1) {
+  connection.query("UPDATE ?? SET ? WHERE ?", [table, answer2, answer1], function (err) {
     if (err) throw err;
     connection.end();
   })
